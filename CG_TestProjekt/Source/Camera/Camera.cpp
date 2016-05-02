e3b18c8281e9d16628acccd6877ad953e0915525
sonczeckf <fa.sonczeck@ostfalia.de>
2016-05-02 09:21:10 +0200

    Camera class added

---
+++ CG_TestProjekt/Source/Camera/Camera.cpp
@@ -0,0 +1,44 @@
+#include "Camera.h"
+
+using namespace FancyMath;
+
+Camera::Camera(Vector3f inEyeVector, Vector3f inViewTarget, Vector3f inUpVector)
+{
+	eyeVector = inEyeVector;
+	viewTarget = inViewTarget;
+	upVector = inUpVector;
+}
+
+Camera::~Camera()
+{
+}
+
+Matrix4 Camera::GetViewMatrix()
+{
+	Matrix4 rotation = Matrix4(1);
+	Matrix4 translation = Matrix4(1);
+
+	Vector3f viewDirection = Vector3f::Normalize(Camera::eyeVector - Camera::viewTarget);
+	Vector3f rightVector = Vector3f::Normalize(Vector3f::CrossProduct(Camera::upVector, viewDirection));
+	Vector3f cameraUpVector = Vector3f::CrossProduct(viewDirection, rightVector);
+	
+	rotation.Elements[0] = rightVector.X;
+	rotation.Elements[1] = cameraUpVector.X;
+	rotation.Elements[2] = viewDirection.X;
+
+	rotation.Elements[4] = rightVector.Y;
+	rotation.Elements[5] = cameraUpVector.Y;
+	rotation.Elements[6] = viewDirection.Y;
+
+	rotation.Elements[8] = rightVector.Z;
+	rotation.Elements[9] = cameraUpVector.Z;
+	rotation.Elements[10] = viewDirection.Z;
+
+	translation.Elements[12] = Camera::eyeVector.X * -1;
+	translation.Elements[13] = Camera::eyeVector.Y * -1;
+	translation.Elements[14] = Camera::eyeVector.Z * -1;
+	
+	Matrix4 retVal = rotation * translation;
+	
+	return retVal;
+}
---
+++ CG_TestProjekt/Source/Camera/Camera.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "FancyLib\FancyMath.h"
+
+class Camera
+{
+
+private:
+	FancyMath::Vector3f eyeVector;
+	FancyMath::Vector3f viewTarget;
+	FancyMath::Vector3f upVector;
+	
+public:
+	Camera(FancyMath::Vector3f inEyeVector, FancyMath::Vector3f inViewTarget, FancyMath::Vector3f inUpVector = FancyMath::Vector3f(0, 1, 0));	
+	~Camera();
+
+public:
+	FancyMath::Matrix4 GetViewMatrix();
+};
+
